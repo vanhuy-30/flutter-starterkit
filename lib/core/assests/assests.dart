4ac5bbe06885163a8b013a2f3daee5c045489630
Ly Van Huy <lyvanhuy.it30@gmail.com>
2025-05-08 09:47:39 +0700

    Add splash screen

--- lib/core/assests/assests.dart
+++ lib/core/assests/assests.dart
@@ -2,7 +2,10 @@
 const icVietnamFlag = 'assets/icons/ic_vietnam_flag.png';
 const icEnglandFlag = 'assets/icons/ic_england_flag.png';
 
-
 // animations
 const loadingAnimation = 'assets/animations/loading_animation.png';
 const notFoundAnimation = 'assets/animations/not_found_animation.png';
+
+// images
+const splashLogo = 'assets/images/splash_logo.png';
+const brandingLogo = 'assets/images/branding.png';
