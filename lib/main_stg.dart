1cc1b247662d4e0aa05ff01c2115601dbcb3fef6
Ly Van Huy <lyvanhuy.it30@gmail.com>
2026-03-20 15:43:16 +0700

    Rename env file

--- lib/main_stg.dart
+++ lib/main_stg.dart
@@ -2,6 +2,6 @@ import 'package:flutter_starter_kit/app/bootstrap/app_bootstrap.dart';
 
 Future<void> main() async {
   await AppBootstrap.run(
-    envFile: "assets/env/.env.stg",
+    envFile: "assets/env/.env.staging",
   );
 }
